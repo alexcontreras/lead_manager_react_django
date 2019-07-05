45a0a8132b71aa4783d5b262fb0abd4c79b694ec
Alex Contreras <ac@jexan.com>
2019-07-04 20:32:26 -0600

    Error handling & alerts

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,4 +1,10 @@
 import { combineReducers } from "redux";
 import leads from "./leads";
+import errors from "./errors";
+import messages from "./messages";
 
-export default combineReducers({ leads });
+export default combineReducers({
+  leads,
+  errors,
+  messages
+});
